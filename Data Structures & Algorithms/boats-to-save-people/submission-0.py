0e409f62d5fe037eb4b3f1ec656c26762c918c17
Mr Goh <138341937+UniversalTze@users.noreply.github.com>
2026-08-24 17:48:55 +1000

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        sortedWeights = sorted(people)
+        # weights are now in ascending order
+        l = len(people) - 1
+        r = 0
+        numboats = 0
+
+        while r <= l:
+            if r == l:
+                numboats += 1
+                break
+            if l == limit or (sortedWeights[l] + sortedWeights[r] > limit):
+                # loop until l is below limit in that it can add weights
+                l -= 1
+                numboats += 1
+                continue
+            if (sortedWeights[l] + sortedWeights[r] == limit):
+                l -= 1
+                r += 1
+                numboats += 1
+                continue
+
+            val = sortedWeights[l] + sortedWeights[r]
+            while val < limit:
+                r += 1
+                val += sortedWeights[r]
+            if val > limit:
+                r -= 1
+            else:
+                r += 1
+            l -= 1
+            numboats += 1
+
+        return numboats
+
+                
+
+
+        
