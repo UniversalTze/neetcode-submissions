10ed9fb37726d66a45b96ccf71363ae846acbbe7
Mr Goh <138341937+UniversalTze@users.noreply.github.com>
2026-08-25 20:45:44 +1000

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        # if n is 3 = [0, 0, 0, 0]
+        dp[0] = 1 # one way to current position
+        dp[1] = 1 # only way to get to step 1 (move 1 step up)
+        for index in range(1, n + 1):
+            dp[index] = dp[index - 1] + dp[index - 2]
+        return dp[n]
+        
