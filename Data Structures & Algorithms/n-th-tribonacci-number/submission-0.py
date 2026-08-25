4a32cd55c24958e6ded491bc6db3911a03ed54dc
Mr Goh <138341937+UniversalTze@users.noreply.github.com>
2026-08-25 20:56:50 +1000

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        # for fibonacci
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+        # 0, 1, 1, 2, 4, 7,
+        return dp[n]
+
+
+        
+        
