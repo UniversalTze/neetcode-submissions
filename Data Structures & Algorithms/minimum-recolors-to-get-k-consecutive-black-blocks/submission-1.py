fe5dbba900bfdad1f146c0b9584b4f347d297d74
Mr Goh <138341937+UniversalTze@users.noreply.github.com>
2026-09-15 14:10:13 +1000

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-1

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        # k is either smaller or equal to block
+        change = 0
+        for index in range(k):
+            if blocks[index] == 'W':
+                change += 1
+        # get the first block (if there is no other strign to parse, 
+        # minim has already been minimised)
+        minim = change
+
+        for index in range(k, len(blocks)):
+            if blocks[index] == 'B' and blocks[index - k] == 'W':
+                change -= 1
+            elif blocks[index] == 'W' and blocks[index - k + 1] == 'B':
+                change += 1
+            minim = min(change, minim)
+
+
+        return minim
+            
+
+        
